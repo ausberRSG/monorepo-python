c58ab72816863f24901f13f0ed01676e1d8b4880
Ausberto Medina <ausberto.medina@rsg.com.co>
2025-04-15 15:01:24 -0500

    fix bux deploy

--- projecta/main.py
+++ projecta/main.py
@@ -1,12 +1,2 @@
 """Entrypoint"""
-
-
-def sum(a: int, b: int):
-    """
-        Funcion que retorna la suma entre 2 numeros
-    """
-    return a+b
-
-
-suma = sum(5, 5)
-print(f"Resultado de la suma es: {suma}")
+print("Projecto A")
